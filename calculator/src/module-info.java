e0a766c607467ee0cd8d7af0dcbab98ce6fe6085
TonyGregg <antonygenil@gmail.com>
2019-11-20 19:56:31 -0600

    Initial draft setup with 2 modules

---
+++ calculator/src/module-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Antony Genil Gregory on 11/20/2019 7:54 PM
+ * For project : Java11Experiments
+ **/module calculator {
+     requires math.util;
+}
---
+++ math.util/src/com/genil/math/MathUtil.java
@@ -0,0 +1,8 @@
+package com.genil.math;
+
+/**
+ * Created by Antony Genil Gregory on 11/20/2019 7:54 PM
+ * For project : Java11Experiments
+ **/
+public class MathUtil {
+}
